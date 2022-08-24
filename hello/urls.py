047ff26852d2b0281af1b6ab36ead7915c4b7436
Sekhaer <atluri_soma@srmap.edu.in>
2022-08-24 20:11:40 +0530

    2nd commit

--- hello/urls.py
+++ hello/urls.py
@@ -3,5 +3,5 @@ from . import views
 from django.urls import path
 urlpatterns = [
     path ("hello/", views.index,name="hello"),
-    path ("hi/hi", views.indexx,name="hello")
+    path ("hi/", views.indexx,name="hello")
 ]
